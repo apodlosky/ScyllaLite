b020308585465e64c0033863bd24002bb3c45641
Adam Podlosky <apodlosky@gmail.com>
2019-12-13 21:15:21 -0700

    Remove configuration file support

--- Scylla/Scylla.cpp
+++ Scylla/Scylla.cpp
@@ -6,29 +6,10 @@
 
 ConfigurationHolder Scylla::config(L"Scylla.ini");
 PluginLoader Scylla::plugins;
-
 ProcessLister Scylla::processLister;
 
 const WCHAR Scylla::DEBUG_LOG_FILENAME[] = L"Scylla_debug.log";
-
 FileLog Scylla::debugLog(DEBUG_LOG_FILENAME);
-ListboxLog Scylla::windowLog;
-
-void Scylla::initAsGuiApp()
-{
-	config.loadConfiguration();
-	plugins.findAllPlugins();
-
-	NativeWinApi::initialize();
-	SystemInformation::getSystemInformation();
-
-	if(config[DEBUG_PRIVILEGE].isTrue())
-	{
-		processLister.setDebugPrivileges();
-	}
-
-	ProcessAccessHelp::getProcessModules(GetCurrentProcess(), ProcessAccessHelp::ownModuleList);
-}
 
 void Scylla::initAsDll()
 {
